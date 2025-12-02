3a22694864c0e64fc17279ad46cf95b45c759f65
Arpit Maurya <arpitbabu802@gmail.com>
2025-12-03 05:22:27 +0530

    fix issue

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,6 @@ app.use(cors({
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 }));
-app.options("*", cors());
 
 app.use(express.json({limit:'10mb'}))
 app.use(express.urlencoded({ extended: true , limit:'10mb'}))
