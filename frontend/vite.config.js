30d4d3194265ea2e85c61460838e5a6dd55affbc
Arpit Maurya <arpitbabu802@gmail.com>
2025-12-03 12:16:56 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,12 +7,4 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-
-  preview: {
-    host: true,       // required
-    port: 4173,       // render detects this
-    allowedHosts: [
-      "chatui-2.onrender.com"   // your frontend domain
-    ]
-  }
 })
